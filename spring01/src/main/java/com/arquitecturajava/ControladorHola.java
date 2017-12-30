1edb6e7ffe534609263df8b73846915aeb0176a1
Jose Antonio <joseant.prieto@outlook.com>
2017-12-30 08:22:09 +0100

    spring01

---
+++ spring01/src/main/java/com/arquitecturajava/ControladorHola.java
@@ -0,0 +1,15 @@
+package com.arquitecturajava;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ControladorHola {
+
+	@RequestMapping("/hola")
+	public String hola() {
+
+		return "hola2";
+	}
+
+}
